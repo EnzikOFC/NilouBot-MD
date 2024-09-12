da957a6c610218da384d3ef7534e80a7ccf22758
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-12 14:28:31 -0500

    Actualizar mods-banchat.js

--- plugins/mods-banchat.js
+++ plugins/mods-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `🌹 *El Bot Ha Sido Desactivado En Este Chat*`, m, rcanal, )
+conn.reply(m.chat, `🌹 *La Bot Ah Sido Desactivada En Este Chat*`, m, rcanal, )
 
 }
 handler.help = ['banchat']
