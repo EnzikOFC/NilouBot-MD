6461f307d475ffe8bc59184be948ff666529a782
모┊ᴊᴏsᴛɪɴ┊ᴼᶠᶜ ჯ <142342932+Jostin207@users.noreply.github.com>
2024-09-12 13:46:51 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -185,8 +185,8 @@ body: null,
 sourceUrl: redes, 
 thumbnail: icons }}}) 
 m.react('✅')
-    let pp = 'https://qu.ax/DMoL.mp4'
-    let pp2 = 'https://qu.ax/dYK.mp4'
+    let pp = 'https://qu.ax/olEc.mp4'
+    let pp2 = 'https://qu.ax/olEc.mp4'
 
 conn.sendMessage(m.chat, { video: { url: [pp, pp2].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: null })
 
