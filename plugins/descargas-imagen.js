53acc2a04635be55151b39b72f5c3694c2471c99
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 12:07:55 -0500

    Actualizar descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -10,7 +10,41 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   const res = await googleImage(text);
   const image = await res.getRandom();
   const link = image;
-  conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*\n> ${textbot}`, m, null, rcanal);
+  //conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*\n> ${textbot}`, m, null, rcanal);
+const messages = [[ // CARRUSEL 1
+'Descripción de Carrusel 1', 
+'Footer de Carrusel 1',
+link,
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[null, null]],
+[[]],
+[[]]
+], [ // CARRUSEL 2
+'Descripción de Carrusel 2',
+'Footer de Carrusel 2',
+random1(),
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[]],
+[[]],
+[[]]
+], [ // CARRUSEL 3
+'Descripción de Carrusel 3',
+'Footer de Carrusel 3',
+random1(),
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[]],
+[[]],
+[[]]
+], [ // CARRUSEL 4
+'Descripción de Carrusel 4',
+'Footer de Carrusel 4',
+random1(),
+[['Botón1', usedPrefix + 'menu'] /* etc... */],
+[[]],
+[[]],
+[[]]
+]] /* etc... */
+await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m)     
 };
 handler.help = ['imagen <query>'];
 handler.tags = ['buscador', 'tools', 'descargas'];
