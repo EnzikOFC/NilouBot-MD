8689b4c64b856e7cd84857d16b5f22e9346bf50f
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 10:59:39 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-    img = imagen1
+   let img = imagen1
 
   let chat = global.db.data.chats[m.chat]
 
