65ac316c8e65498482a6f3d171c7dceb754d3e30
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 13:34:56 -0500

    Actualizar descargas-imagen.js

--- plugins/descargas-imagen.js
+++ plugins/descargas-imagen.js
@@ -1,49 +1,17 @@
 import {googleImage} from '@bochilteam/scraper';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Nilou*`;
-  conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
-  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-  title: packname,
-  body: wm,
-  previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
-  const res = await googleImage(text);
-  const image = await res.getRandom();
-  //conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*\n> ${textbot}`, m, null, rcanal);
-const messages = [[ // CARRUSEL 1
-'Imagen 1', 
-dev,
-await res.getRandom(),
-[[]],
-[[]],
-[[]],
-[[]]
-], [ // CARRUSEL 2
-'Imagen 2',
-dev,
-await res.getRandom(),
-[[]],
-[[]],
-[[]],
-[[]]
-], [ // CARRUSEL 3
-'Imagen 2',
-dev,
-await res.getRandom(),
-[[]],
-[[]],
-[[]],
-[[]]
-], [ // CARRUSEL 4
-'Imagen 4',
-dev,
-await res.getRandom(),
-[[]],
-[[]],
-[[]],
-[[]]
-]]
-await conn.sendCarousel(m.chat, '🚩 Buscador - Imagenes', `🔍 Resultado de: ${text}`, text, messages, m)     
+if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Nilou*`;
+conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const res = await googleImage(text);
+const image = await res.getRandom();
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, '🚩 Buscador - Imagenes', `🔍 Resultado de: ${text}`, text, messages, m); 
 };
 handler.help = ['imagen <query>'];
 handler.tags = ['buscador', 'tools', 'descargas'];
