34c1d9be654c7068cf10ff763d22298e233c8b97
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-16 20:15:01 -0500

    Actualizar info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -7,12 +7,6 @@ if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
 conn.reply(m.chat, `🚩 ¡Hola! Soy Nilou, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }
-if (/^Akira$/i.test(m.text)) {
-conn.reply(m.chat, `*El Mejor Hosting* ❤️‍🩹`, m, rcanal, )
-}
-if (/^Nilou$/i.test(m.text)) {
-conn.reply(m.chat, `*Gracias Por Conocerme.* 🫂`, m, rcanal, )
-}
 return !0;
 };
 export default handler;
